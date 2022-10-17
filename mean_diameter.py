fa1f1ce051c920a8f0a132d516d40bd645bdc7d0
mdegiulio97 <mdegiulio97@gmail.com>
2022-10-17 14:39:11 +0200

    mean time/diameter curve

--- mean_diameter.py
+++ mean_diameter.py
@@ -4,30 +4,30 @@ import pandas as pd
 import math
 import sys
 
-#open file csv and plot the file
-df = pd.read_csv(r"test2.csv", sep=";", decimal=',' , encoding="latin-1" )
+# Open file csv and plot the file
+df = pd.read_csv(r"test2.csv", sep=";", decimal=',', encoding="latin-1")
 
-DiaTime = df ['Diastolic Time [sec]'] #diastolic time (time of min)
+DiaTime = df['Diastolic Time [sec]']# Diastolic time (time of min)
 DiaTime = np.array(DiaTime)
 DiaTime = DiaTime[~np.isnan(DiaTime)]
 
-SysTime = df ['Systolic Time [sec]'] #systolic time (time of max)
+SysTime = df['Systolic Time [sec]']# Systolic time (time of max)
 SysTime = np.array(SysTime)
 
-EndPulse = df['End Pulse Time [sec]'] #cycle end point
+EndPulse = df['End Pulse Time [sec]']# Cycle end point
 EndPulse = np.array(EndPulse)
 EndPulse = EndPulse[~np.isnan(EndPulse)]
 
-Time = df['Tempo [ms]'] #time of simulation
+Time = df['Tempo [ms]']# Time of simulation
 Time = np.array(Time)
 
-Diameter = df['Diametro [mm]'] #Diameter value for each instant of time
+Diameter = df['Diametro [mm]']# Diameter value for each instant of time
 Diameter = np.array(Diameter)
 
-diameterDia = df['Diametro Diastolico [mm]'] #Diameter diastolic
+diameterDia = df['Diametro Diastolico [mm]']# Diameter diastolic
 diameterDia = np.array(diameterDia)
 
-diameterSys = df['Diametro Sistolico [mm]'] #Diameter systolic
+diameterSys = df['Diametro Sistolico [mm]']# Diameter systolic
 diameterSys = np.array(diameterSys)
 
 period = Time[1] - Time[0]
@@ -44,42 +44,42 @@ for i in range(len(EndPulse)):
     CyclePoint.append((numPoint))
 print('Cycle point :', CyclePoint)
 
-#calculation of the points of each cycle
+# calculation of the points of each cycle
 for i in range(len(CyclePoint)):
     CyclePoint[i] /= period
     CyclePoint[i] = round(CyclePoint[i], 2)
     CyclePoint[i] = int(CyclePoint[i])
-print('The number of points for each cycle are:',CyclePoint)
+print('The number of points for each cycle are:', CyclePoint)
 
-#calculation of mean and median
+# calculation of mean and median
 for i in range(len(CyclePoint)):
     meanPoint = np.mean(CyclePoint)
     meanPoint = round(meanPoint)
     meanPoint = int(meanPoint)
     median = np.median(CyclePoint)
     median = int(median)
-print('The average of the points per cycle is:',meanPoint)
+print('The average of the points per cycle is:', meanPoint)
 print('The median is:', median)
 
-#limit value accept: 15% of median
+# limit value accept: 15% of median
 rate_median = (median*15)/100
 rate_median = round(rate_median)
 print('The range relative to the median in percent is +/-', rate_median)
 
-#delete bad cycle
+# delete bad cycle
 for i in range(len(CyclePoint)):
     if (CyclePoint[i] > (median + rate_median)) or (CyclePoint[i] < (median - rate_median)):
         CyclePoint.pop(i)
-        DiaTime = DiaTime.tolist() #covert array in lisr and use 'pop' to remove the bad cycle
+        DiaTime = DiaTime.tolist() #covert array in list and use 'pop' to remove the bad cycle
         DiaTime.pop(i)
 
 max_CyclePoint = max(CyclePoint)
 size_CyclePoint = len(CyclePoint)
 
-#create a empty matrix
+# create a empty matrix
 matrix = [[float('NaN')]*max_CyclePoint for i in range(size_CyclePoint)]
 
-#matrix filling
+# matrix filling
 time_index = 0
 for n in range(len(DiaTime)):
     while Time[time_index] < (DiaTime[n]-0.001):
@@ -89,22 +89,23 @@ for n in range(len(DiaTime)):
         time_index += 1
 print(matrix)
 
-#change matrix to array and calculate mean og column
+# change matrix to array and calculate mean og column
 matrix = np.array(matrix)
 meanCol = np.nanmean(matrix, axis=0)
 print(meanCol)
 
-#plot of diameter variation over time
-plt.figure()
+# plot of diameter variation over time
+plt.figure(1)
 plt.plot(Time[:], Diameter[:])
 plt.xlabel("Time [ms]")
 plt.ylabel("Diameter [mm]")
 plt.title("diameter variation over time")
-plt.show()
 
-#plot of the time/diameter curve on average
-plt.figure()
+# plot of the time/diameter curve on average
+plt.figure(2)
 plt.plot(meanCol)
 plt.title("mean time/diameter curve")
 plt.show()
+
+
 
